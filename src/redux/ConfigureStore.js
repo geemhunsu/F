38b5aff38873726ffdd44f7f5edba6c7c68edf49
d0ngwooK1m <dongwookim.dev@gmail.com>
2021-10-20 10:03:23 +0900

    (김동우) FIX ConfigureStore 수정

--- src/redux/ConfigureStore.js
+++ src/redux/ConfigureStore.js
@@ -2,10 +2,16 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { createBrowserHistory } from 'history';
 import { connectRouter } from 'connected-react-router';
+import User from './modules/user';
+import Image from './modules/image';
+import Post from './modules/post';
 
 export const history = createBrowserHistory();
 
 const rootReducer = combineReducers({
+  post: Post,
+  user: User,
+  image: Image,
   router: connectRouter(history),
 });
 
