fcbe89611455f8f5fdc064e496b3cd51e469b163
d0ngwooK1m <dongwookim.dev@gmail.com>
2021-10-22 16:09:57 +0900

    (김동우) ADD 댓글 이미지 수정

--- src/components/Post.js
+++ src/components/Post.js
@@ -15,8 +15,9 @@ import { VscRunAll, VscTriangleDown, VscTriangleUp } from 'react-icons/vsc';
 const Post = () => {
   const inputs = React.useRef([]);
   const dispatch = useDispatch();
-  const userList = useSelector(state => state.user?.userList);
+  const userList = useSelector((state) => state.user?.userList);
   const loginInfo = useSelector((state) => state.user.userId);
+  const userImageUrl = useSelector((state) => state.user.imageUrl);
   const is_me = useSelector((state) => state.user.userId);
   React.useEffect(() => {
     dispatch(postCreators.getPostMiddleware());
@@ -77,10 +78,13 @@ const Post = () => {
                 alignItems='center'
                 padding='20px 10px'
               >
-                <Image shape='circle' src={
-                  val.userImageUrl
-                  // userList.find(user => user.userId === val.userId).imageUrl
-                } />
+                <Image
+                  shape='circle'
+                  src={
+                    val.userImageUrl
+                    // userList.find(user => user.userId === val.userId).imageUrl
+                  }
+                />
                 <Grid width='70%' height='100%'>
                   <Text margin='5px 0px 0px 0px' bold>
                     {`${val.lastName}  ${val.firstName}`}
@@ -531,7 +535,11 @@ const Post = () => {
                   padding='0px 10px'
                   margin='auto'
                 >
-                  <Image shape='circle' margin='10px' src={val.userImageUrl} />
+                  {loginInfo !== null ? (
+                    <Image shape='circle' margin='10px' src={userImageUrl} />
+                  ) : (
+                    <Image shape='circle' margin='10px' />
+                  )}
                   <Grid display='flex' flexDirection='column'>
                     <Input
                       width='78%'
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -25,7 +25,7 @@ const LoginPage = () => {
 
   const login = () => {
     if (userId === '' || password === '') {
-      window.alert('아이디 혹은 비밀번호가 공란입니다! 입력해주세요');
+      return window.alert('아이디 혹은 비밀번호가 공란입니다! 입력해주세요');
     }
 
     const loginInfo = {
@@ -38,17 +38,63 @@ const LoginPage = () => {
 
   return (
     <>
-      <Grid display='block' width='100%' height='100%' fontSize='12px' backgroundColor='#fff' color='#1c1e21' lineHeight='normal'>
-        <Grid className='상단랩' display='flex' width='100%' height='75%' justifyContent='center' backgroundColor='#f0f2f5'>
-          <Grid display='flex' padding='150px 0 150px 0' flexDirection='row' width='960px'>
-            <Grid className='좌측타이틀' boxSizing='border-box;' width='400px' margin='100px 0px 0 0' padding='0 32px 0 0'>
-              <Image margin='0px' shape='TitleLogo' src='https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg'></Image>
+      <Grid
+        display='block'
+        width='100%'
+        height='100%'
+        fontSize='12px'
+        backgroundColor='#fff'
+        color='#1c1e21'
+        lineHeight='normal'
+      >
+        <Grid
+          className='상단랩'
+          display='flex'
+          width='100%'
+          height='75%'
+          justifyContent='center'
+          backgroundColor='#f0f2f5'
+        >
+          <Grid
+            display='flex'
+            padding='150px 0 150px 0'
+            flexDirection='row'
+            width='960px'
+          >
+            <Grid
+              className='좌측타이틀'
+              boxSizing='border-box;'
+              width='400px'
+              margin='100px 0px 0 0'
+              padding='0 32px 0 0'
+            >
+              <Image
+                margin='0px'
+                shape='TitleLogo'
+                src='https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg'
+              ></Image>
               <Grid width='100%' padding='0 0 0 30px'>
-                <Text width='30px'>Facebook에서 전세계에 있는 친구, 가족, 지인들과 함께 이야기를 나눠보세요.</Text>
+                <Text width='30px'>
+                  Facebook에서 전세계에 있는 친구, 가족, 지인들과 함께 이야기를
+                  나눠보세요.
+                </Text>
               </Grid>
             </Grid>
-            <Grid display='flex' flexDirection='column' textAlign='center' margin='0px 50px' width='400px'>
-              <Grid backgroundColor='#ffff' padding='20px' textAlign='center' borderRadius='8px' margin='auto' boxShadow='rgba(0, 0, 0, 0.2) 0px 2px 10px'>
+            <Grid
+              display='flex'
+              flexDirection='column'
+              textAlign='center'
+              margin='0px 50px'
+              width='400px'
+            >
+              <Grid
+                backgroundColor='#ffff'
+                padding='20px'
+                textAlign='center'
+                borderRadius='8px'
+                margin='auto'
+                boxShadow='rgba(0, 0, 0, 0.2) 0px 2px 10px'
+              >
                 <Grid>
                   <Input
                     height='60px'
@@ -61,7 +107,7 @@ const LoginPage = () => {
                     inputFocusBorder='1px solid #1877f2'
                     fontSize='20px'
                     placeholder='이메일 또는 전화번호'
-                    _onChange={e => {
+                    _onChange={(e) => {
                       console.log(e.target.value);
                       setUserId(e.target.value);
                     }}
@@ -86,7 +132,7 @@ const LoginPage = () => {
                   placeholder='비밀번호'
                   fontSize='20px'
                   type='password'
-                  _onChange={e => {
+                  _onChange={(e) => {
                     console.log(e.target.value);
                     setPassword(e.target.value);
                   }}
@@ -107,7 +153,15 @@ const LoginPage = () => {
                 </LoginButton>
                 <Text color='#1877f2'>비밀번호를 잊으셨나요?</Text>
                 <Hr />
-                <Button text='새 계정 만들기' _onClick={modalOpen} fontSize='17px' width='40%' margin='20px' backgroundColor='#42b72a' borderRadius='7px' />
+                <Button
+                  text='새 계정 만들기'
+                  _onClick={modalOpen}
+                  fontSize='17px'
+                  width='40%'
+                  margin='20px'
+                  backgroundColor='#42b72a'
+                  borderRadius='7px'
+                />
                 <LogInModal showModal={showModal} setShowModal={setShowModal} />
               </Grid>
               <Grid width='100%' padding='20px 0 0 30px'>
@@ -120,12 +174,24 @@ const LoginPage = () => {
         </Grid>
       </Grid>
       <Grid textAlign='center' width='60%' margin='auto'>
-        <Grid height='20%' backgroundColor='#fff' minWidth='60%' margin='auto' padding='20px'>
-          <Text>한국어 English (US) Tiếng Việt Bahasa Indonesia ภาษาไทย Español 中文(简体) 日本語 Português (Brasil) Français (France) Deutsch</Text>
+        <Grid
+          height='20%'
+          backgroundColor='#fff'
+          minWidth='60%'
+          margin='auto'
+          padding='20px'
+        >
+          <Text>
+            한국어 English (US) Tiếng Việt Bahasa Indonesia ภาษาไทย Español
+            中文(简体) 日本語 Português (Brasil) Français (France) Deutsch
+          </Text>
           <hr />
           <Text>
-            가입하기 로그인 Messenger Facebook LiteWatch 사람 페이지 페이지 카테고리 장소 게임 위치 Marketplace Facebook Pay 그룹 채용 정보 Oculus Portal Instagram 지역 기부 캠페인 서비스 투표 정보
-            센터 정보 광고 만들기 페이지 만들기 개발자 채용 정보 개인정보처리방침 쿠키 AdChoices 이용 약관 고객 센터 설정활동 로그
+            가입하기 로그인 Messenger Facebook LiteWatch 사람 페이지 페이지
+            카테고리 장소 게임 위치 Marketplace Facebook Pay 그룹 채용 정보
+            Oculus Portal Instagram 지역 기부 캠페인 서비스 투표 정보 센터 정보
+            광고 만들기 페이지 만들기 개발자 채용 정보 개인정보처리방침 쿠키
+            AdChoices 이용 약관 고객 센터 설정활동 로그
           </Text>
         </Grid>
       </Grid>
